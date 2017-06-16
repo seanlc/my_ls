e282e5df21a49f37536fe5aba7171b4ce8fdc6c5
Sean <seanlcarstensen@gmail.com>
2017-06-16 10:12:40 -0700

    removed unneeded variables

--- my_ls.c
+++ my_ls.c
@@ -14,57 +14,57 @@ void error(char * msg)
     perror(msg);
     exit(-1);
 }
-void add_file_type(struct stat *  mystat, char * str)
+void add_file_type(struct stat *  mystat)
 {
     S_ISDIR(mystat->st_mode) ? printf("d") : printf("-"); 
 }
-void add_owner_perm(struct stat * mystat, char * str)
+void add_owner_perm(struct stat * mystat)
 {
     mystat->st_mode & S_IRUSR ? printf("r") : printf("-");
     mystat->st_mode & S_IWUSR ? printf("w") : printf("-");
     mystat->st_mode & S_IXUSR ? printf("x") : printf("-");   
 }
-void add_group_perm(struct stat * mystat, char * str)
+void add_group_perm(struct stat * mystat)
 {
     mystat->st_mode & S_IRGRP ? printf("r") : printf("-");
     mystat->st_mode & S_IWGRP ? printf("w") : printf("-");
     mystat->st_mode & S_IXGRP ? printf("x") : printf("-"); 
 }
-void add_other_perm(struct stat * mystat, char * str)
+void add_other_perm(struct stat * mystat)
 {
     mystat->st_mode & S_IROTH ? printf("r") : printf("-");
     mystat->st_mode & S_IWOTH ? printf("w") : printf("-");
     mystat->st_mode & S_IXOTH ? printf("x") : printf("-");
 }
-void add_space(char * str)
+void add_space()
 {
     printf(" ");
 }
-void add_mode_str(struct stat * mystat, char * mode)
+void add_mode_str(struct stat * mystat)
 {
-    add_file_type(mystat, mode);
-    add_owner_perm(mystat, mode);
-    add_group_perm(mystat, mode);
-    add_other_perm(mystat, mode);
-    add_space(mode);
+    add_file_type(mystat);
+    add_owner_perm(mystat);
+    add_group_perm(mystat);
+    add_other_perm(mystat);
+    add_space();
 }
-void add_num_links(struct stat * mystat, char * str)
+void add_num_links(struct stat * mystat)
 {
     int n = mystat->st_nlink;
     printf("%d", n);
-    add_space(str);
+    add_space();
 }
-void add_size(struct stat * mystat, char * str)
+void add_size(struct stat * mystat)
 {
     int n = mystat->st_size;
     printf("%d", n);
-    add_space(str);
+    add_space();
 }
-void add_name(struct dirent * myFile, char * str)
+void add_name(struct dirent * myFile)
 {
     printf("%s", myFile->d_name);
 }
-void add_time(struct stat * mystat, char * str)
+void add_time(struct stat * mystat)
 {
     time_t time = mystat->st_mtime;
     char buf[40];
@@ -83,18 +83,18 @@ void add_group()
     g = getgrgid(getuid());
     printf("%s ", g->gr_name);
 }
-void add_mode_links_own_grp(struct stat * mystat, char * fileStr)
+void add_mode_links_own_grp(struct stat * mystat)
 {
-    add_mode_str(mystat,fileStr);
-    add_num_links(mystat, fileStr);
+    add_mode_str(mystat);
+    add_num_links(mystat);
     add_owner();
     add_group();
 }
-void add_size_time_name(struct dirent ** myFile, struct stat * mystat, char * fileStr)
+void add_size_time_name(struct dirent ** myFile, struct stat * mystat)
 {
-    add_size(mystat, fileStr);
-    add_time(mystat, fileStr);
-    add_name(*myFile, fileStr);
+    add_size(mystat);
+    add_time(mystat);
+    add_name(*myFile);
     printf("\n");
 }
 void make_full_path(char * path, char * dirname, char * filename)
@@ -119,20 +119,19 @@ void list_files(char * path, int indent)
     DIR * dirp;
     struct dirent * myFile;
     struct stat mystat;
-    char filestr[1024] = "";    
     char filepath[256] = "";
     if((dirp = opendir(path)) == NULL )
 	error("opendir");
     while((myFile = readdir(dirp)) != NULL )
     {
-        if(strcmp(myFile->d_name, ".") == 0 || strcmp(myFile->d_name, "..") == 0)
+        if(strcmp(myFile->d_name, ".") == 0 || strcmp(myFile->d_name, "..") == 0 || strncmp(myFile->d_name, ".", 1) == 0)
 	    continue;
 	make_indent(indent);
 	make_path(filepath, path, myFile);
 	if( stat(filepath, &mystat) == -1  )
 	    error("stat");
-	add_mode_links_own_grp(&mystat, filestr);
-	add_size_time_name(&myFile, &mystat, filestr);
+	add_mode_links_own_grp(&mystat);
+	add_size_time_name(&myFile, &mystat);
 	if(S_ISDIR(mystat.st_mode))
 	    list_files(filepath, ++indent);
     }
